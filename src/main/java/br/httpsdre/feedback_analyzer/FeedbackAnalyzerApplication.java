b860df09c9d81fa9ed667f4470d881d525a6fb9c
https-dre <diaso.andre@outlook.com>
2025-12-30 19:11:52 -0300

    chore: add application starter (created by spring initializr)

---
+++ src/main/java/br/httpsdre/feedback_analyzer/FeedbackAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package br.httpsdre.feedback_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedbackAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedbackAnalyzerApplication.class, args);
+	}
+
+}
