58ee8eb3d110038fc88b9543e531f012de68db1d
https-dre <diaso.andre@outlook.com>
2025-12-30 19:27:08 -0300

    chore: add database entities with jpa

---
+++ src/main/java/br/httpsdre/feedback_analyzer/models/Batch.java
@@ -0,0 +1,40 @@
+package br.httpsdre.feedback_analyzer.models;
+
+import br.httpsdre.feedback_analyzer.types.BatchStatus;
+import com.github.f4b6a3.uuid.UuidCreator;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = {"id"})
+@Table(name = "analysis_batch")
+@Entity
+public class Batch {
+  @Id
+  private UUID id;
+  @Column(name = "tenant_id")
+  private String tenantId;
+  @Enumerated(EnumType.STRING)
+  private BatchStatus status;
+  @Column(name = "created_at")
+  private LocalDateTime createdAt;
+  @Column(name = "completed_at")
+  private LocalDateTime completedAt;
+  @OneToMany(mappedBy = "batch", cascade = CascadeType.ALL)
+  private List<Feedback> feedbacks;
+
+  public Batch(String tenantId, List<Feedback> feedbacks) {
+    this.id = UuidCreator.getTimeOrderedEpoch();
+    this.tenantId = tenantId;
+    this.status = BatchStatus.PENDING;
+    this.createdAt = LocalDateTime.now();
+    this.feedbacks = feedbacks;
+  }
+}
---
+++ src/main/java/br/httpsdre/feedback_analyzer/models/Feedback.java
@@ -0,0 +1,42 @@
+package br.httpsdre.feedback_analyzer.models;
+
+import br.httpsdre.feedback_analyzer.types.Category;
+import br.httpsdre.feedback_analyzer.types.Sentiment;
+import com.github.f4b6a3.uuid.UuidCreator;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = {"id"})
+@Entity
+@Table(name = "feedback_message")
+public class Feedback {
+  @Id
+  private UUID id;
+  @Column(name = "original_body", columnDefinition = "TEXT")
+  private String originalBody;
+  @Enumerated(EnumType.STRING)
+  private Category category;
+  @Enumerated(EnumType.STRING)
+  private Sentiment sentiment;
+  @Column(columnDefinition = "TEXT")
+  private String summary;
+  @Column(name = "is_analyzed")
+  private boolean isAnalyzed;
+  @ManyToOne
+  @JoinColumn(name = "batch_id")
+  private Batch batch;
+
+  public Feedback(String content) {
+    this.id = UuidCreator.getTimeOrderedEpoch();
+    this.originalBody = content;
+    this.category = Category.UNKNOWN;
+    this.sentiment = Sentiment.UNDEFINED;
+    this.isAnalyzed = false;
+  }
+}
