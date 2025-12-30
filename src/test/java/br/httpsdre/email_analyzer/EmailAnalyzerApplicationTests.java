415a716096cf4788f3668648a6c9e2b42504b03c
https-dre <diaso.andre@outlook.com>
2025-12-30 19:12:40 -0300

    test: add test files (created by spring initializr)

---
+++ src/test/java/br/httpsdre/email_analyzer/EmailAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.httpsdre.email_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
