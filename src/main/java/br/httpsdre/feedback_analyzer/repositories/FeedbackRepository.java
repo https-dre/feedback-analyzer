dad6a417ce54e5c12b7e88a2122c0ad6337ac08f
https-dre <diaso.andre@outlook.com>
2025-12-31 00:52:58 -0300

    feat: add feedback repository

--- src/main/java/br/httpsdre/feedback_analyzer/repositories/FeedbackRepository.java
+++ src/main/java/br/httpsdre/feedback_analyzer/repositories/FeedbackRepository.java
@@ -1,4 +1,12 @@
 package br.httpsdre.feedback_analyzer.repositories;
 
-public interface FeedbackRepository {
+import br.httpsdre.feedback_analyzer.models.Feedback;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface FeedbackRepository extends JpaRepository<Feedback, UUID> {
+  Page<Feedback> findByBatchId(UUID batchId, Pageable pageable);
 }
