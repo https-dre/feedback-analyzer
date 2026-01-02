ad8fb2120917649abd8f3c880ff10c441496bc56
https-dre <diaso.andre@outlook.com>
2026-01-01 23:22:27 -0300

    refactor: make the listener a spring loaded component

--- src/main/java/br/httpsdre/feedback_analyzer/batch/listeners/BatchCompletionListener.java
+++ src/main/java/br/httpsdre/feedback_analyzer/batch/listeners/BatchCompletionListener.java
@@ -2,34 +2,41 @@ package br.httpsdre.feedback_analyzer.batch.listeners;
 
 import br.httpsdre.feedback_analyzer.models.Batch;
 import br.httpsdre.feedback_analyzer.repositories.BatchRepository;
+import br.httpsdre.feedback_analyzer.types.AnalysisStatus;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobExecutionListener;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
+@Component
+@RequiredArgsConstructor
 @Slf4j
 public class BatchCompletionListener implements JobExecutionListener {
-  @Autowired
   private BatchRepository batchRepository;
 
   @Override
   public void afterJob(JobExecution jobExecution) {
     String batchIdStr = jobExecution.getJobParameters().getString("batchId");
+    if (batchIdStr == null) {
+      log.warn("Job finished without Batch ID");
+      return;
+    }
     UUID batchId = UUID.fromString(batchIdStr);
 
-    if(jobExecution.getStatus() == BatchStatus.COMPLETED) {
-      log.info("JOB FINALIZADO COM SUCESSO! Batch ID: {}", batchId);
+    if (jobExecution.getStatus() == BatchStatus.COMPLETED) {
+      log.info("Job Finished Successfuly with Batch ID: {}", batchId);
 
       Batch batch = batchRepository.findById(batchId).orElseThrow();
-      batch.setStatus(br.httpsdre.feedback_analyzer.types.BatchStatus.COMPLETED);
+      batch.setStatus(AnalysisStatus.COMPLETED);
       batchRepository.save(batch);
 
-      log.info("Enviando WebHook para o cliente...");
+      log.info("Sending WebHook...");
     } else {
-      log.error("JOB FALHOU! Status: {}", jobExecution.getStatus());
+      log.error("Job Fail! Status: {}", jobExecution.getStatus());
     }
   }
 }
