c242e8ffb62d9cc199e5793704568140d7eb2924
https-dre <diaso.andre@outlook.com>
2026-01-02 16:02:23 -0300

    chore: add server context-path ('/v1')

--- src/main/java/br/httpsdre/feedback_analyzer/controllers/BatchController.java
+++ src/main/java/br/httpsdre/feedback_analyzer/controllers/BatchController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/v1/batches")
+@RequestMapping("/batches")
 public class BatchController {
   @Autowired
   private BatchService service;
