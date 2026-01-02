b4d44c122b0c923f5c6d5976e863411bc9d946c5
https-dre <diaso.andre@outlook.com>
2026-01-02 14:53:52 -0300

    refactor: replace 'RuntimeException' with custom exceptions created with 'RestExceptionHandler'

--- src/main/java/br/httpsdre/feedback_analyzer/application/WebHookService.java
+++ src/main/java/br/httpsdre/feedback_analyzer/application/WebHookService.java
@@ -1,7 +1,10 @@
 package br.httpsdre.feedback_analyzer.application;
 
+import br.httpsdre.feedback_analyzer.exceptions.NotFoundException;
+import br.httpsdre.feedback_analyzer.exceptions.ValidationException;
 import br.httpsdre.feedback_analyzer.models.WebHook;
 import br.httpsdre.feedback_analyzer.repositories.WebHookRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,15 +14,15 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Service
+@AllArgsConstructor
 public class WebHookService {
-  @Autowired
-  private WebHookRepository repository;
+  private final WebHookRepository repository;
 
   @Transactional
   public WebHook createWebHook(String url, String tenantId, String token) {
     Optional<WebHook> webHookWithUrl = this.repository.findByUrl(url);
     if(webHookWithUrl.isPresent()) {
-      throw new RuntimeException("Url already exists.");
+      throw new ValidationException("Url already exists.");
     }
     WebHook webhook = new WebHook(url, tenantId, token);
     this.repository.save(webhook);
@@ -33,7 +36,7 @@ public class WebHookService {
   public WebHook getWebHookById(String id) {
     var search = this.repository.findById(UUID.fromString(id));
     if(search.isEmpty()) {
-      throw new RuntimeException("WebHook not found");
+      throw new NotFoundException("WebHook not found");
     }
     return search.get();
   }
@@ -42,7 +45,7 @@ public class WebHookService {
   public void deleteById(String id) {
     var search = this.repository.findById(UUID.fromString(id));
     if(search.isEmpty()) {
-      throw new RuntimeException("WebHook not found");
+      throw new NotFoundException("WebHook not found");
     }
     this.repository.deleteById(search.get().getId());
   }
